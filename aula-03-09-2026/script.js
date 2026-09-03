cc0c78aed91bd7010451476942db61661ea52fd8
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2026-09-03 20:59:45 -0300

    Create script.js

---
+++ aula-03-09-2026/script.js
@@ -0,0 +1,41 @@
+//criar os preços dos combustíveis
+const precoGasolina = 6.69;
+const precoEtanol = 5.80;
+const precoDiesel = 6.30;
+
+//criar a função para identificar o tipo de combustível
+function atualizarValor(){
+    let tipo = document.getElementById("combustivel").value;
+    console.log(tipo);
+    let litros = document.getElementById("litros").value;
+    console.log(litros);
+    let precoPorLitro; //pegar os preços acima
+    switch (tipo) {
+        case "gasolina":
+            precoPorLitro = precoGasolina;
+            break;
+        case "etanol":
+            //repetir a lógica
+            precoPorLitro = precoEtanol;
+            break;
+        case "diesel":
+            precoPorLitro = precoDiesel;
+            break;
+        default:
+            console.log("escolha uma opção");
+            return;
+    }
+    console.log(precoPorLitro);
+    //chamar a função para calcular
+    calcularValorAbastecimento(precoPorLitro, litros);
+}
+
+//chamar a função
+let tipoCombustivel = document.getElementById("combustivel");
+tipoCombustivel.addEventListener("change", atualizarValor);
+
+//calcular o abastecimento
+function calcularValorAbastecimento(precoCombustivel, litros){
+    let valorTotal = precoCombustivel* litros;
+    document.getElementById("resultado").innerHTML = valorTotal;
+}
